1716c6c927eb4aa784891e76c590582449563765
Hakjun <hiatus9014@gmail.com>
2022-11-09 15:34:38 +0900

    delete 추가

--- src/main/java/com/mustache/bbs2/domain/dto/ArticleDto.java
+++ src/main/java/com/mustache/bbs2/domain/dto/ArticleDto.java
@@ -8,7 +8,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public class ArticleDto {
     private Long id;
-    private  String title;
+    private String title;
     private String content;
 
     public Article toEntity() {
