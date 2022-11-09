0d88b35910366cbd3b8157a899ebd4019e575c26
Hakjun <hiatus9014@gmail.com>
2022-11-09 14:30:10 +0900

    edit,update 추가

--- src/main/java/com/mustache/bbs2/controller/ArticleController.java
+++ src/main/java/com/mustache/bbs2/controller/ArticleController.java
@@ -6,10 +6,7 @@ import com.mustache.bbs2.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -28,7 +25,7 @@ public class ArticleController {
     @GetMapping("/list")//findall한 결과를 list로 넘김
     public String list(Model model) {
         List<Article> articles = articleRepository.findAll();
-        model.addAttribute("articles",articles);
+        model.addAttribute("articles", articles);
         return "list";
     }
 
@@ -42,6 +39,7 @@ public class ArticleController {
     public String createPage() {
         return "new";
     }
+
     @GetMapping("/{id}")
     public String selectSingle(@PathVariable Long id, Model model) {
         Optional<Article> optArticle = articleRepository.findById(id);
@@ -54,12 +52,32 @@ public class ArticleController {
         }
     }
 
+    @GetMapping("/{id}edit")
+    public String edit(@PathVariable Long id, Model model) {
+        Optional<Article> optionalArticle = articleRepository.findById(id);
+        if (!optionalArticle.isEmpty()) {
+            model.addAttribute("article", optionalArticle.get());
+            return "edit";
+        } else {
+            model.addAttribute("message", String.format("%d가 없습니다.", id));
+            return "error";
+        }
+    }
+
     @PostMapping("") // /articles
     public String articles(ArticleDto articleDto) {
         log.info(articleDto.getTitle());
         Article savedArticle = articleRepository.save(articleDto.toEntity());
         log.info("generatedId:{}", savedArticle.getId());
 
-        return String.format("redirect:/articles/%d",savedArticle.getId());
+        return String.format("redirect:/articles/%d", savedArticle.getId());
+    }
+
+    @PostMapping("/{id}/update")
+    public String update(@PathVariable Long id, ArticleDto articleDto,Model model) {
+        log.info("title:{} content{}", articleDto.getTitle(), articleDto.getContent());
+        Article article = articleRepository.save(articleDto.toEntity());
+        model.addAttribute("article", article);
+        return String.format("redirect:/articles/%d",article.getId());
     }
 }
--- src/main/java/com/mustache/bbs2/domain/Article.java
+++ src/main/java/com/mustache/bbs2/domain/Article.java
@@ -1,5 +1,6 @@
 package com.mustache.bbs2.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +10,7 @@ import javax.persistence.*;
 @Table(name = "article2")
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class Article {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)//db에 아이디생성을 맡기겠다
